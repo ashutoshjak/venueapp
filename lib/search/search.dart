28ab40abbed00a292f9374c6f9f72e49e9814b65
ashutoshjak <58728879+ashutoshjak@users.noreply.github.com>
2021-04-15 17:43:16 +0545

    search modified

--- lib/search/search.dart
+++ lib/search/search.dart
@@ -42,44 +42,49 @@ class SearchVenue extends SearchDelegate {
     final suggestionList = query.isEmpty
         ? ven
         : ven
-        .where((element) =>
-        element.district.toString().toLowerCase().startsWith(query))
-        .toList();
+            .where((element) =>
+                element.venueName.toString().toLowerCase().startsWith(query) ||
+                element.district.toString().toLowerCase().startsWith(query) ||
+                element.address.toString().toLowerCase().startsWith(query) ||
+                element.price.toString().toLowerCase().startsWith(query))
+            .toList();
 
-
-    return query.isEmpty ?  Center(
-        child: Image.asset("assets/images/venue.png",
-            width: MediaQuery.of(context).size.width * 0.7,
-            height: MediaQuery.of(context).size.height * 0.7)
-    ): suggestionList.isEmpty
-        ? Center(child: Text("Venue not Found"))
-        : ListView.builder(
-        itemCount: suggestionList.length,
-        itemBuilder: (context, index) {
-          return InkWell(
-              onTap: () {
-                  Navigator.push(
-                      context,
-                      new MaterialPageRoute(
-                          builder: (context) =>
-                              VenueDetailPage(suggestionList[index])));
-                },
-            child: Card(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  Text("\tVenue Name: ${suggestionList[index].venueName}\n"),
-                  SizedBox(
-                    height: 8.0,
-                  ),
-                  Text("\tDistrict: ${suggestionList[index].district}\n"),
-                  SizedBox(
-                    height: 8.0,
-                  ),
-                  Text("\tAddress: ${suggestionList[index].address}\n"),
-                ],
-              ),
+    return query.isEmpty
+        ? Center(
+            child: Image.asset("assets/images/venue.png",
+                width: MediaQuery.of(context).size.width * 0.7,
+                height: MediaQuery.of(context).size.height * 0.7))
+        : suggestionList.isEmpty
+            ? Center(child: Text("Venue not Found"))
+            : ListView.builder(
+                itemCount: suggestionList.length,
+                itemBuilder: (context, index) {
+                  return InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          new MaterialPageRoute(
+                              builder: (context) =>
+                                  VenueDetailPage(suggestionList[index])));
+                    },
+                    child: Card(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                              "\tVenue Name: ${suggestionList[index].venueName}\n"),
+                          SizedBox(
+                            height: 8.0,
+                          ),
+                          Text(
+                              "\tDistrict: ${suggestionList[index].district}\n"),
+                          SizedBox(
+                            height: 8.0,
+                          ),
+                          Text("\tAddress: ${suggestionList[index].address}\n"),
+                        ],
+                      ),
 //            child: ListTile(
 //                title: Text("Venue Name: ${suggestionList[index].venueName}"),
 //                subtitle: ListTile(
@@ -96,9 +101,9 @@ class SearchVenue extends SearchDelegate {
 //                              VenueDetailPage(suggestionList[index])));
 //                }
 //                ),
-            ),
-          );
-        });
+                    ),
+                  );
+                });
   }
 
   @override
@@ -106,12 +111,12 @@ class SearchVenue extends SearchDelegate {
     // TODO: implement buildSuggestions
 
 //    only added this
-    return query.isEmpty ?  Center(
-        child: Image.asset("assets/images/venue.png",
-            width: MediaQuery.of(context).size.width * 1.5,
-            height: MediaQuery.of(context).size.height * 1.5)
-    ): buildResults(context);
-
+    return query.isEmpty
+        ? Center(
+            child: Image.asset("assets/images/venue.png",
+                width: MediaQuery.of(context).size.width * 1.5,
+                height: MediaQuery.of(context).size.height * 1.5))
+        : buildResults(context);
   }
 }
 
@@ -139,86 +144,161 @@ class VenueDetailPage extends StatelessWidget {
         ),
         backgroundColor: Colors.white,
         body: SingleChildScrollView(
-         child: Card(
-           elevation: 0.0,
-           child: Column(
-             crossAxisAlignment:CrossAxisAlignment.start ,
-             children: <Widget>[
-               Image.network(
-                 venu.image,
-                 height: MediaQuery.of(context)
-               .size
-               .height *
-               0.30,
-                 width:MediaQuery.of(context)
-                     .size
-                     .width *
-                     1,
-               ),
-               SizedBox(
-                 height: 20,
-               ),
-               Row(
-                 children: <Widget>[
-                   Text("\t\t\tDistrict : ",style: TextStyle(fontSize: 19.0,fontWeight: FontWeight.bold,),),
-                   Text(venu.district,style: TextStyle(fontSize: 18.0,),),
-                 ],
-               ),
-               SizedBox(
-                 height: 10,
-               ),
-              Row(
-                children: <Widget>[
-                  Text("\t\t\tAddress : ",style: TextStyle(fontSize: 19.0,fontWeight: FontWeight.bold,),),
-                  Text(venu.address,style: TextStyle(fontSize: 18.0,),),
-                ],
-              ),
-               SizedBox(
-                 height: 10,
-               ),
-               Row(
-                 children: <Widget>[
-                   Text("\t\t\tOpening Time : ",style: TextStyle(fontSize: 19.0,fontWeight: FontWeight.bold,),),
-                   Text(venu.openTime.toString(),style: TextStyle(fontSize: 18.0,),),
-                 ],
-               ),
-               SizedBox(
-                 height: 10,
-               ),
-               Row(
-                 children: <Widget>[
-                   Text("\t\t\tClosing Time : ",style: TextStyle(fontSize: 19.0,fontWeight: FontWeight.bold,),),
-                   Text(venu.closingTime.toString(),style: TextStyle(fontSize: 18.0,),),
-                 ],
-               ),
-               SizedBox(
-                 height: 10,
-               ),
-               Row(
-                 children: <Widget>[
-                   Text("\t\t\tPrice : ",style: TextStyle(fontSize: 19.0,fontWeight: FontWeight.bold,),),
-                   Text(venu.price.toString(),style: TextStyle(fontSize: 18.0,),),
-                 ],
-               ),
-               SizedBox(
-                 height: 10,
-               ),
-               Text("\t\t\tDescription : ",style: TextStyle(fontSize: 19.0,fontWeight: FontWeight.bold,),),
-               SizedBox(
-                 height: 8,
-               ) ,
-               Padding(
-                 padding: const EdgeInsets.only(left: 40),
-                 child: Text(venu.description,style: TextStyle(fontSize: 18.0),),
-               ),
-             ],
-           ),
-
-
-         ),
-        )
-    );
+          child: Card(
+            elevation: 0.0,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Image.network(
+                  venu.image,
+                  height: MediaQuery.of(context).size.height * 0.30,
+                  width: MediaQuery.of(context).size.width * 1,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: <Widget>[
+                    Text(
+                      "\t\t\tDistrict : ",
+                      style: TextStyle(
+                        fontSize: 19.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      venu.district,
+                      style: TextStyle(
+                        fontSize: 18.0,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: <Widget>[
+                    Text(
+                      "\t\t\tAddress : ",
+                      style: TextStyle(
+                        fontSize: 19.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      venu.address,
+                      style: TextStyle(
+                        fontSize: 18.0,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: <Widget>[
+                    Text(
+                      "\t\t\tOpening Time : ",
+                      style: TextStyle(
+                        fontSize: 19.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      venu.openTime.toString(),
+                      style: TextStyle(
+                        fontSize: 18.0,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: <Widget>[
+                    Text(
+                      "\t\t\tClosing Time : ",
+                      style: TextStyle(
+                        fontSize: 19.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      venu.closingTime.toString(),
+                      style: TextStyle(
+                        fontSize: 18.0,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: <Widget>[
+                    Text(
+                      "\t\t\tPrice : ",
+                      style: TextStyle(
+                        fontSize: 19.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text("NRs" + "\t" +
+                      venu.price.toString(),
+                      style: TextStyle(
+                        fontSize: 18.0,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "\t\t\tDescription : ",
+                  style: TextStyle(
+                    fontSize: 19.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 40),
+                  child: Text(
+                    venu.description,
+                    style: TextStyle(fontSize: 18.0),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: <Widget>[
+                    Text(
+                      "\t\t\tWebsite : ",
+                      style: TextStyle(
+                        fontSize: 19.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(
+                        venu.website.toString(),
+                        style: TextStyle(
+                          fontSize: 18.0,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
-
-
